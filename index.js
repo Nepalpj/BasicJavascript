9e2f846151bbc08fdddf76e3828ac69fc6e21ef6
Nepalpj <prawwwe22@gmail.com>
2023-05-30 15:55:25 +0545

    add BasicJavascript

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log("hello")
+
+const length = prompt("enter length")
+const width = prompt("enter width")
+const area = length*width;
+
+console.log(area);
+alert("the area of rectangle is :" + area);
+document.write("The area of rectangle is :" + area);
